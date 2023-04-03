a8ce3e383fde43d8fc1bad93eff73ebf433db8d6
Paulo Vitor <patoassado@gmail.com>
2023-04-03 17:04:56 -0300

    my data

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,6 +1,6 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "samuelleao",
-  whatsappNumber: "+5541111111111",
+  nameUser: "Paulo Vitor",
+  githubUser: "Paulo-Rambo",
+  linkedinUser: "paulo-vitor-ramos-locatelli",
+  whatsappNumber: "+551499704982",
 };
